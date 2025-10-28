29abb21be3edec9924b33f55202f36ce4b7588ff
sofia59 <sofiazupel4@gmail.com>
2025-10-28 19:00:05 -0300

    modifico

--- backend-ecommerce/src/index.ts
+++ backend-ecommerce/src/index.ts
@@ -1,20 +1,18 @@
-// import type { Core } from '@strapi/strapi';
-
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
-
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register({ strapi }) {
+    // Ruta raíz para mostrar algo rápido
+    strapi.server.routes([
+      {
+        method: 'GET',
+        path: '/',
+        handler: async (ctx) => {
+          ctx.body = '🚀 Strapi está funcionando correctamente en Render!';
+        },
+        config: {
+          auth: false,
+        },
+      },
+    ]);
+  },
+  bootstrap() {},
 };
