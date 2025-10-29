cf4a8f496e5f8b0d686670f01a95b99db1df72a0
Kevin <kevindoolzani@gmail.com>
2025-10-28 21:42:35 -0300

    arreglo index

--- backend-ecommerce/src/index.ts
+++ backend-ecommerce/src/index.ts
@@ -1,18 +1,7 @@
-export default {
-  register({ strapi }) {
-    // Ruta raíz para mostrar algo rápido
-    strapi.server.routes([
-      {
-        method: 'GET',
-        path: '/',
-        handler: async (ctx) => {
-          ctx.body = '🚀 Strapi está funcionando correctamente en topooo';
-        },
-        config: {
-          auth: false,
-        },
-      },
-    ]);
-  },
-  bootstrap() {},
+module.exports = {
+  
+  register(/*{ strapi }*/) {},
+
+ 
+  bootstrap(/*{ strapi }*/) {},
 };
