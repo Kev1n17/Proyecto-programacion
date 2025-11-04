711cd9526ce12212ce595282e72a83867002a611
Kevin <kevindoolzani@gmail.com>
2025-11-04 15:27:54 -0300

    Revert "agrego productos completo"
    This reverts commit 8ddc4943a8bb6aa4fde8b4666971b0436f4bb640.

--- backend-ecommerce/types/generated/contentTypes.d.ts
+++ backend-ecommerce/types/generated/contentTypes.d.ts
@@ -626,7 +626,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     slug: Schema.Attribute.UID<'productName'>;
     stock: Schema.Attribute.Integer;
     subCategory: Schema.Attribute.Enumeration<
-      ['Collar', 'Correa', 'Ropa', 'Bebedero', 'Comedero']
+      ['Collar', 'Correa', 'Ropa', 'Comedero', 'Bebedero']
     >;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
